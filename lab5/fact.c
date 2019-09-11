ceb86c20f35a46291d8a3e0c820981302243342f
Rajath-S <39858842+Rajath-S@users.noreply.github.com>
2019-09-11 09:57:58 +0530

    Add files via upload
    added lab5 files

---
+++ lab5/fact.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int i,num,j=1;
+	printf("Enter the number");
+	scanf("%d",&num);
+	for(int i=1;i<num;i++)
+		j=j*i;
+	printf("the factorial of %d is %d\n",num,j);
+}
